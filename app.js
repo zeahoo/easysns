8d110dde82fd447c74a2d91b6fbf8b9fbaa474fa
Sunny Zheng <zeahoooo@gmail.com>
2016-09-17 22:44:48 +0800

    add simple router

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 var http = require('http')
+var parseUrl = require('url').parse
+function homeController (req, res) {
+  res.end('home')
+}
 
-var server = http.createServer(function(req, res){
-  res.end('hello world\n')
+function userController (req, res) {
+  res.end('user')
+}
+
+function notFoundController (req, res) {
+  res.writeHead(404)
+  res.end('404')
+}
+const rules = [
+  {path: '/', controller: homeController},
+  {path: '/user', controller: userController}
+]
+function find (ary, match) {
+  for (var i = 0; i < ary.length; i++) {
+    if (match(ary[i])) return ary[i]
+  }
+}
+var server = http.createServer(function (req, res) {
+  var urlInfo = parseUrl(req.url)
+  var rule = find(rules, function (rule) {
+    return rule.path === urlInfo.pathname
+  })
+  var controller = rule && rule.controller(req, res) || notFoundController
+  controller(req, res)
 })
 
 server.listen(3000)
