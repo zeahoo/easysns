c1d2f4e5894c6b447a880001b8e9245a6f7a0d95
Sunny Zheng <zeahoooo@gmail.com>
2016-09-19 12:30:58 +0800

    add send.js

--- controllers/static.js
+++ controllers/static.js
@@ -1,3 +1,8 @@
+const sendFile = require('../utils/send').sendFile
+const joinPath = require('path').join
+const publicPath = joinPath(__dirname, '../public')
 module.exports = function(req, res) {
-  res.end('static')
+  var path = req.params[1]
+  path = joinPath(publicPath, path)
+  sendFile(path, res)
 }
